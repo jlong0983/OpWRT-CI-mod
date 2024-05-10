61f867e7f40e045bcbc1dbbf11ab7bf336ac6407
jlong0983 <43898243+jlong0983@users.noreply.github.com>
2024-05-10 11:58:44 +0800

    Delete Scripts/luci-app-mosdns.sh

--- Scripts/luci-app-mosdns.sh
+++
@@ -1,6 +0,0 @@
-# drop mosdns and v2ray-geodata packages that come with the source
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
